dd438a19b4f82d396d68c872cd7c568509323036
royalygames <clemgc33@gmail.com>
2023-05-31 16:35:03 +0200

    update index.js

--- asset/index.js
+++ asset/index.js
@@ -1,22 +1,21 @@
 console.log("chargement terminer")
 
-/*récupération du statut de la chaîne*/
+/*récupération du statut de la chaîne de Kirby*/
 
 $.getJSON("")
 .done(addStream)
 .fail(function(){
-    console.log("erreur lors de la récupération du statut");
+    console.log("erreur lors de la récupération du statut de Kirby");
 })
 
 function addStream(data){
-    reponse = data
+    reponse1 = data
 }
 
-/*définition de l'html streamer-kirbys*/
+/*définition de l'html streamer-kirby*/
 
 const streamkirby = `
 <h1>KIRBY 360</h1>
-<h3 id="streamkirby"></h3>
 <img src="./asset/kirby.png" alt="">
 <div class="streamer-button">
     <button><a href="">Twitch</a></button>
@@ -27,7 +26,6 @@ const streamkirby = `
 const streamkirbyOnline = `
 <h1>En ligne</h1>
 <h1>KIRBY 360</h1>
-<h3 id="streamkirby"></h3>
 <img src="./asset/kirby.png" alt="">
 <div class="streamer-button">
     <button><a href="">Twitch</a></button>
@@ -38,7 +36,6 @@ const streamkirbyOnline = `
 const streamkirbyOffline = `
 <h1>hort ligne</h1>
 <h1>KIRBY 360</h1>
-<h3 id="streamkirby"></h3>
 <img src="./asset/kirby.png" alt="">
 <div class="streamer-button">
     <button><a href="">Twitch</a></button>
@@ -46,15 +43,15 @@ const streamkirbyOffline = `
 </div>
 `
 
-function checkValue(reponse) {
-    if (reponse === "online")
-        online = true
+function checkValue(reponse1) {
+    if (reponse1 === "online")
+        online1 = true
         else
-            online = false
+            online1 = false
 }
 
-function checkValue(online) {
-    if (online === ture)
+function checkValue(online1) {
+    if (online1 === ture)
         document.querySelector('.streamer-kirby').innerHTML = streamkirbyOnline;
         else
             document.querySelector('.streamer-kirby').innerHTML = streamkirbyOffline;
@@ -62,3 +59,66 @@ function checkValue(online) {
     }
 
     document.querySelector('.streamer-kirby').innerHTML = streamkirby;
+
+    /*récupération du statut de la chaîne de Erafly*/
+
+    $.getJSON("")
+    .done(addStream)
+    .fail(function(){
+        console.log("erreur lors de la récupération du statut de Erafly");
+    })
+    
+    function addStream(data){
+        reponse2 = data
+    }
+    
+    /*définition de l'html streamer-era*/
+    
+    const streamera = `
+    <h1>ERAFLY</h1>
+    <img src="./asset/era.png" alt="">
+    <div class="streamer-button">
+        <button><a href="">Twitch</a></button>
+        <button><a href="">Youtube</a></button>
+    </div>
+    `
+    
+    const streameraOnline = `
+    <h1>En ligne</h1>
+    <h1>ERAFLY</h1>
+    <img src="./asset/era.png" alt="">
+    <div class="streamer-button">
+        <button><a href="">Twitch</a></button>
+        <button><a href="">Youtube</a></button>
+    </div>
+    `
+    
+    const streameraOffline = `
+    <h1>hort ligne</h1>
+    <h1>ERAFLY</h1>
+    <img src="./asset/era.png" alt="">
+    <div class="streamer-button">
+        <button><a href="">Twitch</a></button>
+        <button><a href="">Youtube</a></button>
+    </div>
+    `
+
+    /*ajout du html via le script*/
+    
+    function checkValue(reponse2) {
+        if (reponse2 === "online")
+            online2 = true
+            else
+                online2 = false
+    }
+    
+    function checkValue(online2) {
+        if (online2 === ture)
+            document.querySelector('.streamer-era').innerHTML = streameraOnline;
+            else
+                document.querySelector('.streamer-era').innerHTML = streameraOffline;
+    
+        }
+    
+        document.querySelector('.streamer-era').innerHTML = streamera;
+       
